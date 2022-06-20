bd8967a333771c6cb86a864d7cd1cd143faf441b
henrykironde <henrykironde@gmail.com>
2022-06-20 19:37:54 -0400

    Add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,6 @@
-"""
-    Dummy conftest.py for neonwranglerpy.
+import pytest
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
 
-# import pytest
+def test_directory_structure():
+    """Initial Tests"""
+    assert True
