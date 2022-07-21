6d0288fd277ea393ad6e9f02cfd9892d16bbc1e2
Nageshbansal <nageshbansal59@gmail.com>
2022-07-21 18:17:07 +0530

    adds stackbytable function
    Signed-off-by: Nageshbansal <nageshbansal59@gmail.com>

--- neonwranglerpy/lib/stackByTable.py
+++ neonwranglerpy/lib/stackByTable.py
@@ -1,5 +1,21 @@
-def stack_by_table(filepath="./stack"):
-    """
-    :param filepath:
-    :return: stacked files
-    """
+import re
+from os import path
+from neonwranglerpy.lib.tools import create_temp, copy_zips
+
+
+def stack_by_table(filepath="./stack", savepath=".", dpID=None, package=None):
+    if not path.exists(filepath):
+        return f"{filepath} doesn't exists "
+
+    if not re.match("DP[1-4]{1}.[0-9]{5}.00[0-9]{1}", dpID):
+        return f"{dpID} is not a properly formatted data product ID. The correct format is DP#.#####.00#, " \
+               f"where the first placeholder must be between 1 and 4."
+
+    # TODO: add check for data should be stacked
+    # TODO: add check for dpID and Package of files
+    # TODO: add check for AOP data
+
+    tempdir = create_temp(savepath)
+
+    if tempdir:
+        copy_zips(filepath, dst=savepath)
