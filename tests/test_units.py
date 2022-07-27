29849634df8f5233de7db522bd8f49262e87f5d4
henry senyondo <henrykironde@gmail.com>
2022-07-27 13:45:33 -0400

    Test coverage report creation

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,6 +1,11 @@
 import pytest
-
+from neonwranglerpy import __version__
 
 def test_directory_structure():
     """Initial Tests"""
     assert True
+    
+    
+def test_version():
+    """Test import version"""
+    assert __version__.startswith("v")
