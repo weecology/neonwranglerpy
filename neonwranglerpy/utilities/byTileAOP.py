3e9a4cc092b9957462a95fd8bba34feea75d7d6a
Nageshbansal <nageshbansal59@gmail.com>
2022-08-23 04:35:00 +0530

    Adds checks for BLAN site
    Signed-off-by: Nageshbansal <nageshbansal59@gmail.com>

--- neonwranglerpy/utilities/byTileAOP.py
+++ neonwranglerpy/utilities/byTileAOP.py
@@ -5,6 +5,7 @@ import re
 from urllib.error import HTTPError
 from urllib.request import urlretrieve
 import pandas as pd
+import geopandas as gpd
 
 from neonwranglerpy import get_data
 from neonwranglerpy.utilities.tools import get_api
@@ -21,7 +22,29 @@ def load_shared_flights():
 
 
 def by_tile_aop(dpID, site, year, easting, northing, savepath=None):
-    """Download the AOP data for given tiles coordinates."""
+    """Download the AOP data for given tiles coordinates.
+
+    Parameters:
+    ----------
+    dpID: str
+        The NEON Data Product ID to be downloaded, in the form DPL.PRNUM.REV
+        e.g. DP3.30006.001
+
+    site : str
+        The NEON site code for a single NEON site e.g. DELA
+
+    year : str
+        The single year to search data e.g 2017
+
+    easting : int, list of int, pandas dataframe of int
+        The easting UTM coordinates of the locations to be downloaded
+
+    northing : int, list of int, pandas dataframe of int
+        The northing UTM coordinates of the locations to be downloaded
+
+    savepath : str, optional
+        The full path to the folder in which the files would be placed locally.
+    """
     if not re.match("DP[1-4]{1}.[0-9]{5}.00[0-9]{1}", dpID):
         return f"{dpID} is not a properly formatted data product ID. The correct format" \
                f" is DP#.#####.00#, where the first placeholder must be between 1 and 4."
@@ -47,11 +70,44 @@ def by_tile_aop(dpID, site, year, easting, northing, savepath=None):
     for i in range(len(response['data']['siteCodes'])):
         if response['data']['siteCodes'][i]['siteCode'] == site:
             all_urls = response['data']['siteCodes'][i]['availableDataUrls']
-        if not len(all_urls):
-            return f"There is no data for {site}"
+    if not len(all_urls):
+        return f"There is no data for {site}"
     month_urls = [x for x in all_urls if re.search(year, x)]
     if not len(month_urls):
         print(f"There is no data for site {site} and year {year}")
+
+    # convert the easting and northing for BLAN site
+    if site == "BLAN":
+        if isinstance(easting, (int, list)):
+            easting = pd.DataFrame({'easting': easting}).reset_index(drop=True)
+            northing = pd.DataFrame({'northing': northing}).reset_index(drop=True)
+
+        df18N_mask = easting <= 250000
+        df17N_mask = easting > 250000
+        if df18N_mask['easting'].any:
+            # df17N dataframe
+            df17N_easting = easting.loc[df17N_mask['easting']].reset_index(drop=True)
+            df17N_northing = northing.loc[df17N_mask['easting']].reset_index(drop=True)
+            df17N = pd.concat([df17N_easting, df17N_northing], axis=1).reset_index(drop=True)
+            df17N.columns = ['easting', 'northing']
+
+            # df18N dataframe
+            df18N_easting = easting.loc[df18N_mask['easting']].reset_index(drop=True)
+            df18N_northing = northing.loc[df18N_mask['easting']].reset_index(drop=True)
+            df18N = pd.concat([df18N_easting, df18N_northing], axis=1).reset_index(drop=True)
+            df18N.columns = ['easting', 'northing']
+
+            # convert the 18N to 17N
+            gdf18N = gpd.GeoDataFrame(df18N, geometry=gpd.points_from_xy(df18N.easting, df18N.northing), crs=32618)
+            df18N_in17nm = gdf18N.to_crs(32617)
+
+            # update the easting and northing for 18N with 17N
+            df18N_in17nm = pd.concat([df18N_in17nm['geometry'].x, df18N_in17nm['geometry'].y], axis=1).reset_index(drop=True)
+            df18N_in17nm.columns = ['easting', 'northing']
+
+            # append df18N and df17N
+            _df = pd.concat([df18N_in17nm, df17N])
+            easting, northing = _df.easting, _df.northing
 
     # TODO : use vectorized operation instead
     tile_easting = [math.floor(float(x / 1000)) * 1000 for x in easting]
