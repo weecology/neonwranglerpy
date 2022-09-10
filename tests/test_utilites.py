973d690fe55854ceb67e75dc72da4e8226190213
Nageshbansal <nageshbansal59@gmail.com>
2022-09-10 21:36:27 +0530

    Fixes flake8 issues
    Signed-off-by: Nageshbansal <nageshbansal59@gmail.com>

--- tests/test_utilites.py
+++ tests/test_utilites.py
@@ -21,33 +21,42 @@ test_urls = [
     ('test_neon_url', 'https://data.neonscience.org/api/v0/products/DP1.10098.001'),
 ]
 
-test_recent_pub = [
-    ("test_get_recent_publication", ['NEON.D08.DELA.DP1.10098.001.variables.20220104T182335Z.csv',
-                                     'NEON.D08.DELA.DP1.10098.001.variables.20220104T175959Z.csv'],
-     'NEON.D08.DELA.DP1.10098.001.variables.20220104T182335Z.csv')
-]
+test_recent_pub = [("test_get_recent_publication", [
+    'NEON.D08.DELA.DP1.10098.001.variables.20220104T182335Z.csv',
+    'NEON.D08.DELA.DP1.10098.001.variables.20220104T175959Z.csv'
+], 'NEON.D08.DELA.DP1.10098.001.variables.20220104T182335Z.csv')]
 
 test_loadByProduct = [
-    ("VST_DELA_2021_2022", "DP1.10098.001", "DELA", "2021-01", "2022-01", [False, True],
-     {'columns':
-         ['uid', 'namedLocation', 'date', 'eventID', 'domainID', 'siteID',
-       'plotID', 'subplotID', 'nestedSubplotID', 'pointID', 'stemDistance',
-       'stemAzimuth', 'recordType', 'individualID',
-       'supportingStemIndividualID', 'previouslyTaggedAs',
-       'samplingProtocolVersion', 'taxonID', 'scientificName', 'taxonRank',
-       'identificationReferences', 'morphospeciesID', 'morphospeciesIDRemarks',
-       'identificationQualifier', 'remarks', 'measuredBy', 'recordedBy',
-       'dataQF'],
-      'data':['45603b3d-ea0b-4022-a4a0-6168e6ceb647', 'DELA_046.basePlot.vst', '2015-06-08', 'vst_DELA_2015', 'D08', 'DELA', 'DELA_046', 21.0, 2.0, 41.0, 11.1, 201.5, 0, 'NEON.PLA.D08.DELA.04068', 0, 0, 'NEON.DOC.000987vE', 'ACRU', 'Acer rubrum L.', 'species', 0, 0, 0, 0, 0, 'mwiegmann@neoninc.org', 'calvis@field-ops.org', 0]}),
-
+    ("VST_DELA_2021_2022", "DP1.10098.001", "DELA", "2021-01", "2022-01", [False, True], {
+        'columns': [
+            'uid', 'namedLocation', 'date', 'eventID', 'domainID', 'siteID', 'plotID',
+            'subplotID', 'nestedSubplotID', 'pointID', 'stemDistance', 'stemAzimuth',
+            'recordType', 'individualID', 'supportingStemIndividualID',
+            'previouslyTaggedAs', 'samplingProtocolVersion', 'taxonID', 'scientificName',
+            'taxonRank', 'identificationReferences', 'morphospeciesID',
+            'morphospeciesIDRemarks', 'identificationQualifier', 'remarks', 'measuredBy',
+            'recordedBy', 'dataQF'
+        ],
+        'data': [
+            '45603b3d-ea0b-4022-a4a0-6168e6ceb647', 'DELA_046.basePlot.vst', '2015-06-08',
+            'vst_DELA_2015', 'D08', 'DELA', 'DELA_046', 21.0, 2.0, 41.0, 11.1, 201.5, 0,
+            'NEON.PLA.D08.DELA.04068', 0, 0, 'NEON.DOC.000987vE', 'ACRU',
+            'Acer rubrum L.', 'species', 0, 0, 0, 0, 0, 'mwiegmann@neoninc.org',
+            'calvis@field-ops.org', 0
+        ]
+    }),
 ]
 
 test_checks = [
-    ("DPID_CHECK", "DP1.1098.001", "DELA", "2021-01", "2022-01", [False, True], 'not a properly formatted data product ID'),
-    ("AOP_PRODUCT_CHECK", "DP3.30005.006", "DELA", "2021-01", "2022-01", [False, True], 'remote sensing data product and cannot be loaded'),
-    ("START_DATE_CHECK", "DP1.10098.001", "DELA", "202-01", "2022-01", [False, True], 'startdate and enddate must be either NA or valid dates'),
-    ("END_DATE_CHECK", "DP1.10098.001", "DELA", "2021-01", "202-01", [False, True], 'startdate and enddate must be either NA or valid dates'),
-    ]
+    ("DPID_CHECK", "DP1.1098.001", "DELA", "2021-01", "2022-01", [False, True],
+     'not a properly formatted data product ID'),
+    ("AOP_PRODUCT_CHECK", "DP3.30005.006", "DELA", "2021-01", "2022-01", [False, True],
+     'remote sensing data product and cannot be loaded'),
+    ("START_DATE_CHECK", "DP1.10098.001", "DELA", "202-01", "2022-01", [False, True],
+     'startdate and enddate must be either NA or valid dates'),
+    ("END_DATE_CHECK", "DP1.10098.001", "DELA", "2021-01", "202-01", [False, True],
+     'startdate and enddate must be either NA or valid dates'),
+]
 
 
 def setup_module():
@@ -86,13 +95,20 @@ def test_get_recent_publications(test_name, file_list, expected):
     assert re_pub == expected
 
 
-@pytest.mark.parametrize("test_name, dpID, site, start_date, end_date, args, expected", test_loadByProduct)
+@pytest.mark.parametrize("test_name, dpID, site, start_date, end_date, args, expected",
+                         test_loadByProduct)
 def test_load_by_product(test_name, dpID, site, start_date, end_date, args, expected):
     setup_functions()
     path = raw_dir_files
     save_files = args[0]
     stacked_df = args[1]
-    data_frame = load_by_product(dpID, site, start_date, end_date, path=path, save_files=save_files, stacked_df=stacked_df)
+    data_frame = load_by_product(dpID,
+                                 site,
+                                 start_date,
+                                 end_date,
+                                 path=path,
+                                 save_files=save_files,
+                                 stacked_df=stacked_df)
 
     columns_values = list(data_frame['vst_mappingandtagging'].dtypes.index)
     first_row_data = list(data_frame['vst_mappingandtagging'].fillna(0).iloc[0])
@@ -101,20 +117,29 @@ def test_load_by_product(test_name, dpID, site, start_date, end_date, args, expe
     assert first_row_data == expected['data']
 
 
-@pytest.mark.parametrize("test_name, dpID, site, start_date, end_date, args, expected", test_checks)
-def test_load_by_product_checks(test_name, dpID, site, start_date, end_date, args, expected):
+@pytest.mark.parametrize("test_name, dpID, site, start_date, end_date, args, expected",
+                         test_checks)
+def test_load_by_product_checks(test_name, dpID, site, start_date, end_date, args,
+                                expected):
     setup_functions()
     path = raw_dir_files
     save_files = args[0]
     stacked_df = args[1]
-    out = load_by_product(dpID, site, start_date, end_date, path=path, save_files=save_files, stacked_df=stacked_df)
+    out = load_by_product(dpID,
+                          site,
+                          start_date,
+                          end_date,
+                          path=path,
+                          save_files=save_files,
+                          stacked_df=stacked_df)
     assert expected in out
 
 
-@pytest.mark.parametrize("test_name, dpID, site, start_date, end_date, args, expected", test_checks)
-def test_zips_by_product_checks(test_name, dpID, site, start_date, end_date, args, expected):
+@pytest.mark.parametrize("test_name, dpID, site, start_date, end_date, args, expected",
+                         test_checks)
+def test_zips_by_product_checks(test_name, dpID, site, start_date, end_date, args,
+                                expected):
     setup_functions()
     path = raw_dir_files
     out = zips_by_product(dpID, site, start_date, end_date, savepath=path)
     assert expected in out
-
