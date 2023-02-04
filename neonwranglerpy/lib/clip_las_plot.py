36bb57ae258f3d8a2d2a79cff3bf9f902cd31f52
Sergio Marconi <s.marconi@ufl.edu>
2023-02-04 16:46:49 -0500

    Update clip_las_plot.py

--- neonwranglerpy/lib/clip_las_plot.py
+++ neonwranglerpy/lib/clip_las_plot.py
@@ -2,15 +2,14 @@
 import laspy
 import numpy as np
 
+
 def clip_raster(plt, data_path, buffer, savepath):
     """Given a Laz file, clip it by the size of the input plot.
-    
     Parameters:
     plt = string with at least easting and northing of the center of the target plot
     data_path =  path in which the laz/las file is stored
     buffer = the buffer around the center of the plot (plot size divided by 2)
     savepath =  full path where to store the clipped laz file.
-    
     """
     # open the NEON las tile
     las = laspy.read(data_path)
@@ -27,7 +26,7 @@ def clip_raster(plt, data_path, buffer, savepath):
     clip_y = (miny < las.y) & (maxy > las.y)
     clip_z = (las.header.max[2] > las.z) & (las.header.min[2] < las.z)
 
-    good_indices = np.where(clip_x & clip_y &clip_z)
+    good_indices = np.where(clip_x & clip_y & clip_z)
 
     # clip las around the coordinates
     clip = las.points[good_indices[0]].copy()
@@ -36,8 +35,4 @@ def clip_raster(plt, data_path, buffer, savepath):
     output_file = laspy.LasData(las.header)
     output_file.points = clip
     output_file.write(savepath)
-    return(output_file)
-
-
-
-
+    return (output_file)
